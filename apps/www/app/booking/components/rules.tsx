6e3e263480cfca5b328895a017f7a529ff1907c3
Karlo Mrakovčić <karlomrak@gmail.com>
2023-11-10 15:07:49 +0100

    fix: rules change

--- apps/www/app/booking/components/rules.tsx
+++ apps/www/app/booking/components/rules.tsx
@@ -27,22 +27,61 @@ function Rules() {
           <DialogDescription></DialogDescription>
         </DialogHeader>
         <div className="flex flex-col gap-4">
-          <div>
+          {/* <div>
             • The parking space can only be booked in advance and latest
             <span style={{ fontWeight: "bold" }}> until 4 p.m</span> of the
             current day when booking a spot{" "}
             <span style={{ fontWeight: "bold" }}>for the next day.</span>
-          </div>
-          <div>
-            • You will be able to book the spot for not longer than{" "}
-            <span style={{ fontWeight: "bold" }}>2 days in advance.</span>
-          </div>
+          </div> */}
           <div>
             • All parking spots are numbered.
             <span style={{ fontWeight: "bold" }}>
               You have to park on the exact parking spot indicated by the
               application.
             </span>
+          </div>
+          <div>
+            <span style={{ fontStyle: "italic" }}>
+              • If somebody is parked on your spot,{" "}
+            </span>{" "}
+            please let us know by sending an email to{" "}
+            <a
+              style={{ color: "blue" }}
+              href="mailto:nexibooking-cro@nexigroup.com"
+            >
+              nexibooking-cro@nexigroup.com
+            </a>
+            <span>
+              {" "}
+              so that we can ask them to move or that you can switch places and
+              park on their spot.
+            </span>
+          </div>
+          <div>
+            • You will be able to book the spot for not longer than{" "}
+            <span style={{ fontWeight: "bold" }}>2 days in advance.</span>
+          </div>
+          <div>
+            •
+            <span style={{ fontWeight: "bold" }}>
+              DO NOT park on the spot that is not marked Nexi
+            </span>{" "}
+            unless the app gave you that spot. In case you do, you will be
+            removed from the list of users and
+            <span style={{ fontWeight: "bold" }}>
+              {" "}
+              permanently banned{" "}
+            </span>{" "}
+            from using the garage.
+          </div>
+          <div>
+            • Currently, only internal employees with Zagreb as their primary
+            work location can use the garage. Guests and external employees are
+            not allowed to use the garage.
+          </div>
+          <div>
+            • Next to the underground garage entrance, there is a parking for
+            guests with payment option.
           </div>
         </div>
         <DialogFooter></DialogFooter>
