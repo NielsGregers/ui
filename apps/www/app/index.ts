3040efe12ac62ade3849bee2daad9918afaa801b
Niels Gregers Johansen <niels@jumpto365.com>
2023-11-14 08:54:08 +0300

    fix: removed db call in root layout

--- apps/www/app/index.ts
+++ apps/www/app/index.ts
@@ -85,42 +85,42 @@ export interface ConfigCache {
   key: string
   hostdata: Fields
 }
-
-export async function readConfigData(key: string, hostname: string) {
-
-  const defaultConfig: ConfigCache = {
-    date: new Date(),
-    key,
-    hostdata: {
-      "@odata.etag": "",
-      Title: "Unknown host",
-      LinkTitle: "",
-      TrackingCode: `
-            
-      (function(c,l,a,r,i,t,y){
-    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+const defaultConfig: ConfigCache = {
+  date: new Date(),
+  key:"default",
+  hostdata: {
+    "@odata.etag": "",
+    Title: "Unknown host",
+    LinkTitle: "",
+    TrackingCode: `
+          
+    (function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
 })(window, document, "clarity", "script", "ixwytyo6af");
 `,
-      id: "",
-      ContentType: "",
-      Modified: "",
-      Created: "",
-      AuthorLookupId: "",
-      EditorLookupId: "",
-      _UIVersionString: "",
-      Attachments: false,
-      Edit: "",
-      LinkTitleNoMenu: "",
-      ItemChildCount: "",
-      FolderChildCount: "",
-      _ComplianceFlags: "",
-      _ComplianceTag: "",
-      _ComplianceTagWrittenTime: "",
-      _ComplianceTagUserId: ""
-    }
-  };
+    id: "",
+    ContentType: "",
+    Modified: "",
+    Created: "",
+    AuthorLookupId: "",
+    EditorLookupId: "",
+    _UIVersionString: "",
+    Attachments: false,
+    Edit: "",
+    LinkTitleNoMenu: "",
+    ItemChildCount: "",
+    FolderChildCount: "",
+    _ComplianceFlags: "",
+    _ComplianceTag: "",
+    _ComplianceTagWrittenTime: "",
+    _ComplianceTagUserId: ""
+  }
+};
+export async function readConfigData(key: string, hostname: string) {
+
+
   const accessToken = await getSpAuthToken();
   const getHostResponse = await httpsGetAll<Value>(accessToken,
     `https://graph.microsoft.com/v1.0/sites/christianiabpos.sharepoint.com:/sites/intra365:/lists/Hosts/items?$expand=fields`);
@@ -143,7 +143,7 @@ export async function readConfigData(key: string, hostname: string) {
   return cache;
 }
 export async function refreshConfigCache(key: string, hostname: string) {
-  return
+ 
   console.log("Refreshing Config cache");
   const client = await connect();
   const cache = await readConfigData(key, hostname);
@@ -163,35 +163,40 @@ export async function refreshConfigCache(key: string, hostname: string) {
 }
 
 export async function getConfigCache(host: string) {
+  return  {
+    date: new Date(),
+    key:"default",
+    hostdata: defaultConfig.hostdata
+  };
+// return 
+//   const client = await connect();
+//   const key = KEY + ":" + host;
+//   let cache = await client
+//     .db(DB)
+//     .collection<ConfigCache>("cache")
+//     .findOne({ key });
 
-  const client = await connect();
-  const key = KEY + ":" + host;
-  let cache = await client
-    .db(DB)
-    .collection<ConfigCache>("cache")
-    .findOne({ key });
+//   if (cache) {
+//     const now = new Date();
+//     const diff = now.getTime() - cache.date.getTime();
+//     const diffInMinutes = Math.round(diff / 60000);
+//     if (diffInMinutes > 1) {
+//       await refreshConfigCache(key, host);
+//       cache = await client
+//         // eslint-disable-next-line turbo/no-undeclared-env-vars
+//         .db(process.env.DATABASE)
+//         .collection<ConfigCache>("cache")
+//         .findOne({ key });
+//     }
+//   } else {
+//     await refreshConfigCache(key, host);
+//     cache = await client
+//       // eslint-disable-next-line turbo/no-undeclared-env-vars
+//       .db(process.env.DATABASE)
+//       .collection<ConfigCache>("cache")
+//       .findOne({ key });
+//   }
 
-  if (cache) {
-    const now = new Date();
-    const diff = now.getTime() - cache.date.getTime();
-    const diffInMinutes = Math.round(diff / 60000);
-    if (diffInMinutes > 1) {
-      await refreshConfigCache(key, host);
-      cache = await client
-        // eslint-disable-next-line turbo/no-undeclared-env-vars
-        .db(process.env.DATABASE)
-        .collection<ConfigCache>("cache")
-        .findOne({ key });
-    }
-  } else {
-    await refreshConfigCache(key, host);
-    cache = await client
-      // eslint-disable-next-line turbo/no-undeclared-env-vars
-      .db(process.env.DATABASE)
-      .collection<ConfigCache>("cache")
-      .findOne({ key });
-  }
-
-  await client.close();
-  return cache;
+//   await client.close();
+//   return cache;
 }
